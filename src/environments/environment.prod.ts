25a5aa6caaed1f823134de148ae32814259a3c24
mrhonek <26533268+mrhonek@users.noreply.github.com>
2025-05-12 16:17:28 -0700

    Update API URL to match new Railway deployment domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://phpecom-production.up.railway.app'
+  apiUrl: process.env['API_URL'] || 'https://web-production-11af.up.railway.app/api'
 }; 
